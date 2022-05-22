d3d277a2250dd4aa120a3f1a99ea879a2d487e6d
Marco <migliavacca.marco08@gmail.com>
2022-05-22 17:10:55 +0200

    Do first test of ex 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...arg) {
+    let indexArgToRemove = arr.indexOf(...arg)
+    arr.splice(indexArgToRemove, 1)
+    return arr
 };
 
 // Do not edit below this line
