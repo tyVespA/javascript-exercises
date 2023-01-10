2974858602915bd9f5a1515d6d4db471c5668163
tyVespA <migliavacca.marco08@gmail.com>
2023-01-10 12:28:03 +0100

    Update calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length == 0) return 0
+  let sum = 0
+
+	// for (let i of arr) {
+  //   sum += arr[i]
+  // }
+  // return sum
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i]
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let result = 1;
+  for (let i = 0; i < arr.length; i++) {
+    result *= arr[i]
+  }
+  return result
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  let result = 1
+
+  for(; num2 > 0; num2--) {
+    result = result * num1
+  }
+  return result
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+	for (let i = num; i > 0; i--) {
+    result = result * i
+  }
+  return result
 };
 
 // Do not edit below this line
